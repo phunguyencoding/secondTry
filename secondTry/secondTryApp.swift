d4819dc9ad3390a05084f25f1287e575f8d8846d
phunguyencoding <phunguyen6896@gmail.com>
2022-07-14 19:37:37 +0700

    Initial Commit

---
+++ secondTry/secondTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondTryApp.swift
+//  secondTry
+//
+//  Created by Phu Nguyen Tran on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct secondTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
